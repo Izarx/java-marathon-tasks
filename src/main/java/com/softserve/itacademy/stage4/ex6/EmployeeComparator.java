824e527422377d60466c92e1bc6be8f3840f4396
Ihor Zakharko <igor.zaharko@gmail.com>
2021-03-09 14:38:49 +0200

    Sprint4 ex6 start v2

--- src/main/java/com/softserve/itacademy/stage4/ex6/EmployeeComparator.java
+++ src/main/java/com/softserve/itacademy/stage4/ex6/EmployeeComparator.java
@@ -1,3 +1,11 @@
 package com.softserve.itacademy.stage4.ex6;
 
-public class EmployeeComparator {}
+import java.util.Comparator;
+
+public class EmployeeComparator implements Comparator<Employee> {
+    @Override
+    public int compare(final Employee o1, final Employee o2)
+    {
+        return 0;
+    }
+}
--- src/main/java/com/softserve/itacademy/stage4/ex6/PersonComparator.java
+++ src/main/java/com/softserve/itacademy/stage4/ex6/PersonComparator.java
@@ -1,3 +1,19 @@
 package com.softserve.itacademy.stage4.ex6;
 
-public class PersonComparator {}
+import java.util.Comparator;
+
+public class PersonComparator implements Comparator<Person> {
+    @Override
+    public int compare(final Person o1, final Person o2)
+    {
+        if (o1 != null && o2 != null) {
+            if (!o2.name.equals(o1.name)) {
+                return o1.name.compareTo(o2.name);
+            }
+            else {
+                return o1.age - o2.age;
+            }
+        }
+        else return 1;
+    }
+}
