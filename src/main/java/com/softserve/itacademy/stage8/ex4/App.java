1cfb2a0a45eb64d567fa5812cdf9817f7ce67de4
Ihor Zakharko <igor.zaharko@gmail.com>
2021-03-23 17:04:14 +0200

    Sprint8 ex4

---
+++ src/main/java/com/softserve/itacademy/stage8/ex4/App.java
@@ -0,0 +1,9 @@
+package com.softserve.itacademy.stage8.ex4;
+
+public class App {
+    public static void main(String[] args)
+    {
+        MyThreads.t1.start();
+        MyThreads.t2.start();
+    }
+}
---
+++ src/main/java/com/softserve/itacademy/stage8/ex4/MyThreads.java
@@ -0,0 +1,38 @@
+package com.softserve.itacademy.stage8.ex4;
+
+public class MyThreads {
+    public final static Object den = new Object();
+    public final static Object ada = new Object();
+
+    public volatile static int n;
+    public volatile static int m;
+
+    public static Thread t1 = new Thread() {
+        public void run() {
+            synchronized (this) {
+                for(int i = 0; i < 5; i++, n++)
+                    System.out.println("Thread1 n = " + n);
+            }
+            Thread.yield();
+            synchronized (ada) {
+                for(int i = 0; i < 5; i++, m++)
+                    System.out.println("Thread1 m = " + m);
+                System.out.println( "Thread1 success!");
+            }
+        }
+    };
+
+    public static Thread t2 = new Thread() {
+        public void run() {
+            synchronized (ada) {
+                for(int i = 0; i < 5; i++, m++)
+                    System.out.println("Thread2 m = " + m);
+                synchronized (den) {
+                    for(int i = 0; i < 5; i++, n++)
+                        System.out.println("Thread2 n = " + n);
+                    System.out.println("Thread2 success!");
+                }
+            }
+        }
+    };
+}
