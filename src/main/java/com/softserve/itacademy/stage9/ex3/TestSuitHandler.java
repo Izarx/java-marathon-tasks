c5feef8bfa888bc2cf6998523c3f2f3c3a1447af
Ihor Zakharko <igor.zaharko@gmail.com>
2021-03-25 21:50:17 +0200

    Sprint1 ex3 mocks

---
+++ src/main/java/com/softserve/itacademy/stage9/ex3/TestSuitHandler.java
@@ -0,0 +1,7 @@
+package com.softserve.itacademy.stage9.ex3;
+
+public class TestSuitHandler {
+    public static void run(Class<?> clazz) {
+        
+    }
+}
---
+++ src/main/java/com/softserve/itacademy/stage9/ex3/TestSuite.java
@@ -0,0 +1,5 @@
+package com.softserve.itacademy.stage9.ex3;
+
+public @interface TestSuite {
+    public String[] strings();
+}
