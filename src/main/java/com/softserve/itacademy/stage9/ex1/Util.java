400d72fb92cafd445a2ebc15e0e39af3e9e6d5fb
Ihor Zakharko <igor.zaharko@gmail.com>
2021-03-26 09:34:41 +0200

    Sprint1 ex2 upgrade

--- src/main/java/com/softserve/itacademy/stage9/ex1/Util.java
+++ src/main/java/com/softserve/itacademy/stage9/ex1/Util.java
@@ -4,17 +4,12 @@ import java.time.LocalDate;
 
 public class Util {
     public static void review(String className) {
-        LocalDate date = LocalDate.now();
         try
         {
             if(Class.forName(className).isAnnotationPresent(Review.class))
             {
-                if (!Class.forName(className).getAnnotation(Review.class).date().equals("today")) {
-                    String[] strings = Class.forName(className).getAnnotation(Review.class).date().split("-");
-                    date = LocalDate.of(Integer.parseInt(strings[0]), Integer.parseInt(strings[1]), Integer.parseInt(strings[2]));
-                }
                 System.out.println("Class " + className + " was reviewed "
-                                            + date + " by "
+                                            + date(className) + " by "
                                             + Class.forName(className).getAnnotation(Review.class).reviewer()
                                             + ".");
             }
@@ -26,5 +21,12 @@ public class Util {
         catch(ClassNotFoundException e) {
             System.out.println("Class " + className + " was not found");
         }
+    }
+
+    public static LocalDate date(String className) throws ClassNotFoundException {
+        String[] strings = Class.forName(className).getAnnotation(Review.class).date().split("-");
+        return Class.forName(className).getAnnotation(Review.class).date().equals("today") ?
+                LocalDate.now() :
+                LocalDate.of(Integer.parseInt(strings[0]), Integer.parseInt(strings[1]), Integer.parseInt(strings[2]));
     }
 }
--- src/main/java/com/softserve/itacademy/stage9/ex3/TestSuitHandler.java
+++ src/main/java/com/softserve/itacademy/stage9/ex3/TestSuitHandler.java
@@ -2,6 +2,6 @@ package com.softserve.itacademy.stage9.ex3;
 
 public class TestSuitHandler {
     public static void run(Class<?> clazz) {
-        
+
     }
 }
